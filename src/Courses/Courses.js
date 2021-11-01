44e04200fdabbdd98ad7b341208a31294be62a56
Yin Li <echogame@users.noreply.github.com>
2021-11-01 15:00:56 -0700

    Adds basic GET courselist

--- src/Courses/Courses.js
+++ src/Courses/Courses.js
@@ -1,14 +1,33 @@
 // System imports
 import React from "react";
+import { useResource } from "react-request-hook";
 
 // Styles
 import './Courses.css';
 
 function Courses({user = {}}) {
+  const [courseList, getCourseList] = useResource(user => {
+    return {
+      url: `/jsonapi/v1/courses?email=${user.email}`,
+      method: 'GET'
+    }
+  });
+
+  // todo: add in pagination information
+  React.useEffect(() => {
+    getCourseList(user)
+  }, [])
+
   return (
     <div>
       <div>Hello from courses</div>
       <div>user email: {user.email}</div>
+      <div className="Courses_pagination">
+        [placeholder for pagination]
+      </div>
+      <div className="Courses_courseList">
+        total courses: {courseList.isLoading ? 'loading....' : courseList.data.length}
+      </div>
     </div>
   )
 }
