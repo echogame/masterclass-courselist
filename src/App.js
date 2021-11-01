97d198b364ac11e4e81991e09f3dd6de06c646c1
Yin Li <echogame@users.noreply.github.com>
2021-11-01 14:28:54 -0700

    Adds /courses route with basic component
    Includes redirect to page on any other page

--- src/App.js
+++ src/App.js
@@ -1,31 +1,42 @@
+// System imports
 import React from "react";
 import {
   BrowserRouter as Router,
-  Switch,
+  Link,
+  Redirect,
   Route,
-  Link
+  Switch
 } from "react-router-dom";
 
-import logo from './logo.svg';
+// Styles
 import './App.css';
+
+// Page components
+import Courses from './Courses/Courses';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div className="App_navigation">
+          <ul>
+            <li>
+              <Link to="/courses">Courses</Link>
+            </li>
+          </ul>
+        </div> 
+      
+        <div className="App_content">
+          <Switch>
+            <Route path="/courses">
+              <Courses />
+            </Route>
+
+            {/* Always go to /courses page */}
+            <Route path="/"><Redirect to="/courses" /></Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ src/Courses/Courses.js
@@ -0,0 +1,12 @@
+// System imports
+import React from "react";
+
+// Styles
+import './Courses.css';
+
+
+function Courses() {
+  return <div>Hello from courses</div>
+}
+
+export default Courses;
